47dd0e3c0c7ae695f4d9a10d5f999aede77945fd
SimoSultan <simosultan2020@gmail.com>
2020-10-12 21:10:21 +1000

    added comments to app.js

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # SimoSultans Calculator
 
+See it in action [here](https://simosultan-calculator.netlify.app/)
+
 ![GitHub followers](https://img.shields.io/github/followers/SimoSultan?style=social)  
 ![Twitter Follow](https://img.shields.io/twitter/follow/simo_sultan?style=social)  
 [www.simonmcurran.com](https://www.simonmcurran.com/)  
@@ -8,3 +10,10 @@
 ## Description
 
 As part of one of our daily challenges we were tasked to use TDD to build a calculator. This has ALWAYS been one of my goals with programming, so I wanted to implement a front end for the calculator. So here it is... 
+UPDATE: So I have to be honest, my weakness came through of impatience to code and built the app first. But still have written tests post deploy (currently in progress writing them)
+
+## Stack
+- HTML
+- Bootstrap
+- JavaScript
+
--- app.js
+++ app.js
@@ -14,12 +14,17 @@ var num1 = ""
 var num2 = ""
 var operation = null
 
-// 
+// the function on each button
+// determine input and add relevant string/functions to global vars
 function handleButtonClick() {
+  // get button press
   let input = this.textContent
   switch (input) {
     case '+':
+      // this is to not allow a user to be able to do 3 + 1 + 1
+      // if operation var is not null, then clear the screen so they have to start again
       if (operation === null) {
+        // add the function to the global and updated the input div
         operation = sum
         inputScreen.textContent += ` ${input} ` 
       } else {
@@ -28,6 +33,7 @@ function handleButtonClick() {
       break;
     case '-':
       if (operation === null) {
+        // add the function to the global and updated the input div
         operation = minus
         inputScreen.textContent += ` ${input} ` 
       } else {
@@ -36,6 +42,7 @@ function handleButtonClick() {
       break;
     case '×':
       if (operation === null) {
+        // add the function to the global and updated the input div
         operation = multiply
         inputScreen.textContent += ` ${input} ` 
       } else {
@@ -44,6 +51,7 @@ function handleButtonClick() {
       break;
     case '÷':
       if (operation === null) {
+        // add the function to the global and updated the input div
         operation = divide
         inputScreen.textContent += ` ${input} ` 
       } else {
@@ -51,23 +59,30 @@ function handleButtonClick() {
       }
       break;
     case 'AC':
+      // clear the screen and global vars if they press this button
       clear()
       break;
     case 'DEL':
+      // remove the last character from the input screen
       inputScreen.textContent = removeLastCharFromString(inputScreen.textContent);
+      // check which global var to remove the last string from
       if (operation === null) 
         num1 = removeLastCharFromString(num1);
       else 
         num2 = removeLastCharFromString(num2);
       break;
     case '=':
+      // calculate what the user wants
+      // update the output div
       let result = performOperation(num1, num2, operation)
-      console.log();
       outputScreen.textContent = result
       break;
     default:
+      // this first statement handles the situation where if the a user has calculated something already
+      // and goes to start another calculation, then clear everything for them 
       if (outputScreen.textContent.length > 0) clear()
       inputScreen.textContent += input 
+      // determines which var to put the string into
       if (operation === null) 
         num1 += input
       else 
@@ -76,7 +91,7 @@ function handleButtonClick() {
   }
 }
 
-
+// these functions are fairly obvious
 function sum(a, b) {
   return a + b
 }
@@ -89,13 +104,19 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b
 }
+
 function performOperation(num1, num2, operation) {
+  // first block deals with handling the '=' press if there was no operation set yet, it will just show the first number in the output screen
+  // otherwise it will do the calculation
+  // ensuring that the number in a string is converted to a number first
+  // if no number is given in either var, then it counts as a 0 which was not by intention but I think is appropriate
   if (operation === null)
     return Number(num1)
   else
     return operation(Number(num1),Number(num2))
 }
 function clear() {
+  // clear everything
   outputScreen.textContent = ""
   inputScreen.textContent = ""
   num1 = ""
@@ -103,5 +124,6 @@ function clear() {
   operation = null
 }
 function removeLastCharFromString(str) {
+  // remove the last char from a string
   return str.substring(0, str.length - 1);
 }
